38ae81032a3a60ad96d3161119d7c2db9a36bc08
siarhkul <siarhkul@gmail.com>
2024-05-19 17:20:37 +0200

    feat: Add debug commands3

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -6,25 +6,18 @@ export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-/*    new CodePipeline(this, 'AwesomePipeline', {
-      pipelineName: 'AwesomePipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('SiarhKul/aws-cicd', 'main'),
-        commands:[
-          'npm ci',
-          'npx cdk synth'
-        ]
-      })
 
-    })*/
+    const source = CodePipelineSource
+      .gitHub('SiarhKul/aws-cicd', 'main');
 
     new CodePipeline(this, 'AwesomePipeline', {
+      publishAssetsInParallel: false,
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('SiarhKul/aws-cicd', 'main'),
+        input: source,
         commands: [
           'npm ci',
-          'ls -al cdk.out',
+          'ls',
           'npx cdk synth',
           'ls -al',  // List all files in the current directory
           'ls -al cdk.out'  // List all files in the cdk.out directory
