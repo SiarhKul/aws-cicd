1ba6c4432ac6c38064caf50aa93243d1dec32114
siarhkul <siarhkul@gmail.com>
2024-05-20 08:10:54 +0200

    feat: Add Lambda

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,5 +1,8 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {join} from "path";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 
 interface LambdaStackProps extends StackProps {
   stageName?: string,
@@ -8,5 +11,15 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
+
+    new NodejsFunction(this, 'hello-lambda', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: (join(__dirname, '..', 'services', 'hello.ts')),
+      environment: {
+        STAGE: props.stageName!
+      }
+    })
+
   }
 }
